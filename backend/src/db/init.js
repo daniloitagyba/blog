addc5298a2b55fd18c44e76a2e95fdbfced394dc
Danilo Itagyba <daniloitagyba@gmail.com>
2025-03-13 15:21:17 -0300

    build: setup docker and dependences

--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export function initDatabase() {
   const DATABASE_URL = process.env.DATABASE_URL
   mongoose.connection.on('open', () => {
-    console.info('successfully connected to database:', DATABASE_URL)
+    console.info('successfully connected to database')
   })
   const connection = mongoose.connect(DATABASE_URL)
   return connection
